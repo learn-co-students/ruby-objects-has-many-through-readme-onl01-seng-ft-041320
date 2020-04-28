db52809c27e2d88a6ecc7669f37b271fcacfa1fc
Tanattha <tanattha.thuenim@gmail.com>
2020-04-28 19:22:22 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -40,18 +40,4 @@ class Customer
         tip = total * 0.2
         Meal.new(waiter, self, total, tip)
       end
-
-=begin      
-    def self.oldest_customer
-        oldest_age = 0
-        oldest_customer = nil
-        self.all.each do |customer|
-          if customer.age > oldest_age
-            oldest_age = customer.age
-            oldest_customer = customer
-          end
-        end
-        oldest_customer
-      end
-=end
   end
