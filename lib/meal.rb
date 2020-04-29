b90aead4ea85c14d8f35f9612fb5355440d2873a
markcascardi <mcascardi@gmail.com>
2020-04-28 20:14:54 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
+  attr_accessor :customer, :tip, :total, :waiter
 
+  @@all = []
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @tip = tip
+    @total = total
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
