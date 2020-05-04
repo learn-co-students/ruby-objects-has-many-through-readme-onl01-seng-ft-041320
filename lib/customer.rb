b0da6fdcbae1aaec74deb2a10c8e73ec86e4fc21
jaesonwatts <illender.the.great@gmail.com>
2020-05-03 18:38:30 -0600

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,42 @@
+require "pry"
+
 class Customer
-  
+
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      if self.name== meal.customer.name
+        #binding.pry
+        meal
+      end
+    end
+  end
+
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
+
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,44 @@
+require "pry"
+
 class Waiter
+
+  attr_accessor :name, :years, :meals
+  @@all = []
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+
+  def best_tipper
+
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def meals
+    Meal.all
+  end
+
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+
+    best_tipped_meal.customer
+  end
+
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self #checking for waiter now
+    end
+  end
 
 end
