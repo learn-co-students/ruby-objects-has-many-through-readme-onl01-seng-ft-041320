97aa30391f2f07955a22c43391e9b9c68767fc23
Neil Empey <argus.two.2@gmail.com>
2020-05-04 18:35:50 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,46 @@
+require "pry"
+
 class Customer
-  
+
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      if self.name== meal.customer.name
+        #binding.pry
+        meal
+      end
+    end
+  end
+
+  def waiters
+    array = []
+  Meal.all.each do |meal|
+      if meal.customer == self
+          array << meal.waiter
+      end
+    end
+    #binding.pry
+    array.uniq
+  end
+
+
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+
 end
